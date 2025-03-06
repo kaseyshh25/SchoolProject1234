f826d86625f3ad45ddccaee31c7b7873864f6282
kaseyshh25 <kaseyshh25@gmx.com>
2025-03-06 17:15:49 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int num = (int)(Math.random() * 10 + 1);
+
+        // Print the number to the console
+        System.out.println("The generated number is: " + num);
+    }
+}
