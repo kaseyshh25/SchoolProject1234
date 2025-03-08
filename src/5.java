3d6b940808c8fd47e62050c41df4fec2d1f9b550
kaseyshh25 <kaseyshh25@gmx.com>
2025-03-08 18:33:48 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,8 @@
+package SchoolProject1234;
+
+public class RandomCode {
+  public static void main(String[] args) {
+    int i = (int)(Math.random() * 10);
+    System.out.println("The random number is: " + i);
+  }
+}
